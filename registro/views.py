000679d66aa258286158cd5b4fca2fcfa2d302ca
aosorio <argenisosorio580@hotmail.com>
2016-08-07 21:59:34 -0400

    Primer commit / Proyecto base

---
+++ registro/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.views.generic import TemplateView,ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+
+from registro.models import Persona
+
+class Consultar(ListView):
+    model = Persona
+
+class Registrar(CreateView):
+    model = Persona
+    fields = ['nombre', 'cedula']
+    success_url = reverse_lazy('registro:consultar')
+
+class Editar(UpdateView):
+    model = Persona
+    fields = ['nombre', 'cedula']
+    success_url = reverse_lazy('registro:consultar')
+
+class Borrar(DeleteView):
+    model = Persona
+    success_url = reverse_lazy('registro:consultar')
