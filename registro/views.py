1bf9a020466f5f744033ca728e8597a849c5deb0
aosorio <aosorio@cenditel.gob.ve>
2017-07-03 17:04:38 -0400

    Estable el envio de mensajes de django con las clases de bootstrap

--- registro/views.py
+++ registro/views.py
@@ -12,18 +12,28 @@ class Consultar(ListView):
     model = Persona
 
 
-class Registrar(SuccessMessageMixin,CreateView):
+class Registrar(CreateView):
     model = Persona
     fields = ['nombre', 'cedula']
     success_url = reverse_lazy('registro:consultar')
     success_message = "Se agrego una persona con éxito"
 
+    def form_valid(self, form):
+        self.object = form.save()
+        messages.success(self.request, self.success_message)
+        return super(Registrar, self).form_valid(form)
 
-class Editar(SuccessMessageMixin,UpdateView):
+
+class Editar(UpdateView):
     model = Persona
     fields = ['nombre', 'cedula']
     success_url = reverse_lazy('registro:consultar')
     success_message = "Se actualizo una persona con éxito"
+
+    def form_valid(self, form):
+        self.object = form.save()
+        messages.warning(self.request, self.success_message)
+        return super(Editar, self).form_valid(form)
 
 
 class Borrar(DeleteView):
@@ -32,5 +42,7 @@ class Borrar(DeleteView):
     success_message = "Se elimino una persona con éxito"
 
     def delete(self, request, *args, **kwargs):
-        messages.success(self.request, self.success_message)
-        return super(Borrar, self).delete(request, *args, **kwargs)
+        #messages.success(self.request, self.success_message)
+        #messages.warning(self.request, self.success_message)
+        messages.info(self.request, self.success_message)
+        return super(Borrar, self).delete(self, request, *args, **kwargs)
