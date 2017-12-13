50e7602084ea73f31827fb28992f8c3e3254bba7
Argenis Osorio <aosorio@cenditel.gob.ve>
2017-12-13 02:18:37 -0400

    Agregada instancia para subir archivos desde un formulario

--- registro/urls.py
+++ registro/urls.py
@@ -5,6 +5,7 @@ from registro import views
 
 urlpatterns = patterns('',
     url(r'^$', views.Consultar.as_view(), name='consultar'),
+    url(r'^file$', views.file, name='file'),
     url(r'^registrar$', views.Registrar.as_view(), name='registrar'),
     url(r'^editar/(?P<pk>\d+)$', views.Editar.as_view(), name='editar'),
     url(r'^borrar/(?P<pk>\d+)$', views.Borrar.as_view(), name='borrar'),
--- registro/views.py
+++ registro/views.py
@@ -4,6 +4,21 @@ from django.views.generic import TemplateView,ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from registro.models import Persona
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+def file(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'registro/file.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'registro/file.html')
 
 
 class Consultar(ListView):
