64483a670ae9c794ff0bf1ee4586c9e10852bc3c
aosorio <argenisosorio580@hotmail.com>
2018-07-17 08:38:51 -0400

    Haciendo que los campos del formulario de registro sean requeridos

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -38,32 +38,38 @@ class RegisterForm(UserCreationForm):
     username = forms.CharField(max_length=30, label=("Usuario"), widget=forms.TextInput(attrs={
         'class': 'form-control',
         'style': 'min-width: 0; width: 25%; display: inline;',
-    }))
+        'required': 'true',
+    }), required = True)
 
     first_name = forms.CharField(label=("Nombres"), widget=forms.TextInput(attrs={
         'class': 'form-control',
         'style': 'min-width: 0; width: 25%; display: inline;',
-    }))
+        'required': 'true',
+    }), required = True)
     
     last_name = forms.CharField(label=("Apellidos"), widget=forms.TextInput(attrs={
         'class': 'form-control',
         'style': 'min-width: 0; width: 25%; display: inline;',
-    }))
+        'required': 'true',
+    }), required = True)
 
     email = forms.CharField(label=("Email"), widget=forms.TextInput(attrs={
         'class': 'form-control',
         'style': 'min-width: 0; width: 25%; display: inline;',
-    }))
+        'required': 'true',
+    }), required = True)
 
     password1 = forms.CharField(label=("Contraseña"), widget=forms.PasswordInput(attrs={
         'class': 'form-control',
         'style': 'min-width: 0; width: 25%; display: inline;',
-    }))
+        'required': 'true',
+    }), required = True)
 
     password2 = forms.CharField(label=("Contraseña (confirmación)"), widget=forms.PasswordInput(attrs={
         'class': 'form-control',
         'style': 'min-width: 0; width: 25%; display: inline;',
-    }))
+        'required': 'true',
+    }), required = True)
 
     class Meta:
         model = User
