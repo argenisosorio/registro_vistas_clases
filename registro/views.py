c39aa118ed67c3a3d3d5c4d6db468454f61dc666
Argenis Osorio <argenisosorio580@hotmail.com>
2020-03-09 21:42:55 +0000

    Arreglando detalles

--- registro/views.py
+++ registro/views.py
@@ -7,7 +7,6 @@ from registro.models import Persona
 from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
-import csv
 
 
 class Consultar(ListView):
@@ -29,35 +28,3 @@ class Editar(UpdateView):
 class Borrar(DeleteView):
     model = Persona
     success_url = reverse_lazy('registro:consultar')
-
-
-def subir_csv(request):
-    """
-    Función que permite subir el fichero CSV en la carpeta media.
-    """
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        file = fs.url(filename)
-        return render(request, 'registro/subir_csv.html', {
-            'file': file
-        })
-    return render(request, 'registro/subir_csv.html')
-
-
-def insertar_data_csv(request, template_name='registro/leer_csv.html'):
-    """
-    Función que permite insertar los datos del fichero CSV en la base de datos.
-    """
-    with open(settings.MEDIA_ROOT+'/data.csv', 'r') as listado:
-        datos = csv.reader(listado, delimiter=',') # Separar la data por coma.
-        for row in datos:
-            nombre = row[0]
-            cedula = row[1]
-            #print "----"
-            #print nombre
-            #print cedula
-            Persona.objects.create(nombre=nombre, cedula=cedula)
-    listado.close()
-    return render(request, template_name)
