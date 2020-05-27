039dda106375e9b2e0ec4b12848c6e70a8770b14
Argenis Osorio <argenisosorio580@hotmail.com>
2020-05-27 20:37:20 +0000

    Agregado ejemplo de XMLHttpRequest sencillo

--- registro/urls.py
+++ registro/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 from registro import views
+from django.conf import settings
 
 
 urlpatterns = patterns('',
@@ -8,4 +9,8 @@ urlpatterns = patterns('',
     url(r'^registrar$', views.Registrar.as_view(), name='registrar'),
     url(r'^editar/(?P<pk>\d+)$', views.Editar.as_view(), name='editar'),
     url(r'^borrar/(?P<pk>\d+)$', views.Borrar.as_view(), name='borrar'),
+    # Definiendo la url que va a servir los documentos.
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 )
