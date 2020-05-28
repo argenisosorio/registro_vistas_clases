a1e8db8f6167a5b6dabbbb95963bb84bb85e327b
Argenis Osorio <argenisosorio580@hotmail.com>
2020-05-28 19:33:38 +0000

    Otro ejemplo de post con ajax

--- registro/views.py
+++ registro/views.py
@@ -7,6 +7,7 @@ from registro.models import Persona
 from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from django.http import JsonResponse
 
 
 class Consultar(ListView):
@@ -17,6 +18,18 @@ class Registrar(CreateView):
     model = Persona
     fields = ['nombre', 'cedula']
     success_url = reverse_lazy('registro:consultar')
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests, instantiating a form instance with the passed
+        POST variables and then checked for validity.
+        """
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
 
 
 class Editar(UpdateView):
